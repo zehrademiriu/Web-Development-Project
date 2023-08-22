800a86e34acb928d168d1281e983815a1d789fa0
zehrademiriu <142937142+zehrademiriu@users.noreply.github.com>
2023-08-22 22:28:47 +0300

    tipdetails.mjs
    Component of the front-end.

---
+++ tipdetails.mjs
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+function TipDetails() {
+  const { tipId } = useParams();
+  const [tipDetails, setTipDetails] = useState({});
+
+  useEffect(() => {
+    const fetchTipDetails = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/tips/${tipId}`);
+        setTipDetails(response.data);
+      } catch (error) {
+        console.error('Error fetching tip details:', error);
+      }
+    };
+
+    fetchTipDetails();
+  }, [tipId]);
+
+  return (
+    <div>
+      <h2>Tip Details</h2>
+      <h3>{tipDetails.title}</h3>
+      <p>{tipDetails.content}</p>
+    </div>
+  );
+}
+
+export default TipDetails;
